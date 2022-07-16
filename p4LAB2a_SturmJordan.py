dd6e12d48fc456c2345f68b422db7c0a6715ba89
sturmj6829 <107274009+sturmj6829@users.noreply.github.com>
2022-07-16 15:41:16 -0400

    Add files via upload

---
+++ p4LAB2a_SturmJordan.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
